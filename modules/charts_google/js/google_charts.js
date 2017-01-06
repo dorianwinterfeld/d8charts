9f3443469f0bc601124c83cd38a78aaba8c330bc
Michael Mwebaze <michael.mwebaze@gmail.com>
2017-01-06 11:54:03 -0500

    google chart issue

--- modules/charts_google/js/google_charts.js
+++ modules/charts_google/js/google_charts.js
@@ -9,17 +9,18 @@
             google.charts.load('current', {'packages':['corechart']});
             google.charts.setOnLoadCallback(drawChart);
             var dataTable = $('.chart-google').attr('data-chart');
+            var ggleOptions = $('.chart-google').attr('google-options');
             function drawChart() {
                 var data = google.visualization.arrayToDataTable($.parseJSON(dataTable));
                 // Set chart options
-                var options = {'title':'How Much Pizza I Ate Last Night',
+                /*var options = {'title':'How Much Pizza I Ate Last Night',
                     //'width':600,
                     //'height':400
                 };
-
+*/
                 // Instantiate and draw our chart, passing in some options.
                 var chart = new google.visualization.BarChart(document.getElementById('chart'));
-                chart.draw(data, options);
+                chart.draw(data, ggleOptions);
             }
         }
     }
