43f5c950155f596f93b2f6651fb66e04eac4e850
Daniel Cothran <dcothran@jsi.local>
2017-01-13 14:06:10 -0500

    c3 chart data format change

--- modules/c3/js/charts_c3.js
+++ modules/c3/js/charts_c3.js
@@ -25,12 +25,7 @@
             var chart = c3.generate({
                 bindto: '#chart',
                 data: {
-                    columns: [
-                        // ['data1', 30, 200, 100, 400, 150, 250],
-                        // ['data2', 50, 20, 10, 40, 15, 25]
-                        c3ChartDataSeries[0].data,
-                        c3ChartDataSeries[1].data
-                    ],
+                    columns: c3ChartDataSeries,
                     type: formattedType
                 },
                 bar: {
