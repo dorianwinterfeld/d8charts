b1df9d79e6cca4b53455d467538e651e560d629b
Daniel Cothran <dcothran@jsi.local>
2017-03-02 11:43:37 -0500

    reformat js to use to spaces

--- modules/charts_c3/js/charts_c3.js
+++ modules/charts_c3/js/charts_c3.js
@@ -3,17 +3,17 @@
  * JavaScript integration between C3 and Drupal.
  */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.chartsC3 = {
-        attach: function (context, settings) {
+  Drupal.behaviors.chartsC3 = {
+    attach: function (context, settings) {
 
-            $('.charts-c3').once().each(function () {
-                if ($(this).attr('data-chart')) {
-                    var c3Chart = $('.charts-c3').attr('data-chart');
-                    c3.generate(JSON.parse(c3Chart))
-                }
-            });
+      $('.charts-c3').once().each(function () {
+        if ($(this).attr('data-chart')) {
+          var c3Chart = $('.charts-c3').attr('data-chart');
+          c3.generate(JSON.parse(c3Chart))
         }
+      });
     }
+  }
 }(jQuery));
--- modules/charts_highcharts/js/charts_highcharts.js
+++ modules/charts_highcharts/js/charts_highcharts.js
@@ -3,17 +3,17 @@
  * JavaScript integration between Highcharts and Drupal.
  */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.chartsHighcharts = {
-        attach: function (context, settings) {
+  Drupal.behaviors.chartsHighcharts = {
+    attach: function (context, settings) {
 
-            $('.charts-highchart').once().each(function () {
-                if ($(this).attr('data-chart')) {
-                    var highcharts = $(this).attr('data-chart');
-                    $(this).highcharts(JSON.parse(highcharts));
-                }
-            });
+      $('.charts-highchart').once().each(function () {
+        if ($(this).attr('data-chart')) {
+          var highcharts = $(this).attr('data-chart');
+          $(this).highcharts(JSON.parse(highcharts));
         }
-    };
+      });
+    }
+  };
 }(jQuery));
