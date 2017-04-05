ded4049786b53a36d6ce0cd30fe69dbcc57daf46
Michael Mwebaze <michael.mwebaze@gmail.com>
2017-04-05 17:16:41 -0400

    added service to be used by other modules integrating charts

---
+++ src/Services/ChartsSettingsService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\charts\Services;
+
+use Drupal\Core\Config\ConfigFactory;
+
+class ChartsSettingsService implements ChartsSettingsServiceInterface{
+
+  //private $editableConfigName = 'charts.settings';
+  private $configFactory;
+
+  public function __construct(ConfigFactory $config_factory){
+    $this->configFactory = $config_factory;
+  }
+
+  public function getChartsSettings(){
+    drupal_set_message('Charts settings');
+    $config = $this->configFactory->getEditable('charts.settings');
+
+    return $config->get('charts_default_settings');
+  }
+}
